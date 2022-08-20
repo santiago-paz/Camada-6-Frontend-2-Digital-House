6a78604ec30d52f3b3a099c7a6ebb9c8d384dbcc
Santiago Paz <santiago.paz.1992@gmail.com>
2022-08-20 13:44:53 -0300

    Actualización al logo del DVD rebotando.

--- clase-9/3-animaciones/js/index.js
+++ clase-9/3-animaciones/js/index.js
@@ -1,43 +1,41 @@
 const logoDVD = document.getElementById('dvd_logo');
-let x = 0;
-let y = 0;
+let x = 0, y = 0;
 let direccionX = "derecha"
 let direccionY = "abajo"
 
 setInterval(() => {
-    if (direccionX === "derecha" && direccionY === "abajo") {
+    if (direccionX === "derecha") {
         logoDVD.style.left = `${x++}px`;
-        logoDVD.style.top = `${y++}px`;
+
+        if (direccionY === "abajo") {
+            logoDVD.style.top = `${y++}px`;
+        }
+
+        if (direccionY === "arriba") {
+            logoDVD.style.top = `${y--}px`;
+        }
     }
 
-    if (direccionX === "derecha" && direccionY === "arriba") {
-        logoDVD.style.left = `${x++}px`;
-        logoDVD.style.top = `${y--}px`;
-    }
-
-    if (direccionX === "izquierda" && direccionY === "abajo") {
+    if (direccionX === "izquierda") {
         logoDVD.style.left = `${x--}px`;
-        logoDVD.style.top = `${y++}px`;
+
+        if (direccionY === "abajo") {
+            logoDVD.style.top = `${y++}px`;
+        }
+
+        if (direccionY === "arriba") {
+            logoDVD.style.top = `${y--}px`;
+        }
     }
 
-    if (direccionX === "izquierda" && direccionY === "arriba") {
-        logoDVD.style.left = `${x--}px`;
-        logoDVD.style.top = `${y--}px`;
-    }
+    if (x < 0) direccionX = "derecha"
+    if (y < 0) direccionY = "abajo"
 
-    if (x === 0) {
-        direccionX = "derecha"
-    }
-
-    if (y === 0) {
-        direccionY = "abajo"
-    }
-
-    if (x > window.outerWidth - logoDVD.clientWidth) {
+    if (x > window.innerWidth - logoDVD.clientWidth) {
         direccionX = "izquierda"
     }
 
-    if (y > window.outerHeight - logoDVD.clientHeight) {
+    if (y > window.innerHeight - logoDVD.clientHeight) {
         direccionY = "arriba"
     }
-}, 10);
+}, 1);
