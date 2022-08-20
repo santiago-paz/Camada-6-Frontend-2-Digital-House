e126484c7ebd7983e64e3a572e97190c5bc36a0e
Santiago Paz <santiago.paz.1992@gmail.com>
2022-08-20 20:36:44 -0300

    Update index.js

--- clase-9/3-animaciones/js/index.js
+++ clase-9/3-animaciones/js/index.js
@@ -1,41 +1,8 @@
 const logoDVD = document.getElementById('dvd_logo');
-let x = 0, y = 0;
-let direccionX = "derecha"
-let direccionY = "abajo"
-
+let x = 0, y = 0, dx = "➡", dy = "↓";
 setInterval(() => {
-    if (direccionX === "derecha") {
-        logoDVD.style.left = `${x++}px`;
-
-        if (direccionY === "abajo") {
-            logoDVD.style.top = `${y++}px`;
-        }
-
-        if (direccionY === "arriba") {
-            logoDVD.style.top = `${y--}px`;
-        }
-    }
-
-    if (direccionX === "izquierda") {
-        logoDVD.style.left = `${x--}px`;
-
-        if (direccionY === "abajo") {
-            logoDVD.style.top = `${y++}px`;
-        }
-
-        if (direccionY === "arriba") {
-            logoDVD.style.top = `${y--}px`;
-        }
-    }
-
-    if (x < 0) direccionX = "derecha"
-    if (y < 0) direccionY = "abajo"
-
-    if (x > window.innerWidth - logoDVD.clientWidth) {
-        direccionX = "izquierda"
-    }
-
-    if (y > window.innerHeight - logoDVD.clientHeight) {
-        direccionY = "arriba"
-    }
+    logoDVD.style.left = `${dx === "➡" ? x++ : x--}px`
+    logoDVD.style.top = `${dy === "↓" ? y++ : y--}px`
+    dx = x < 0 ? "➡" : x > (window.innerWidth - logoDVD.clientWidth) ? "←" : dx
+    dy = y < 0 ? "↓" : y > (window.innerHeight - logoDVD.clientHeight) ? "↑" : dy
 }, 1);
